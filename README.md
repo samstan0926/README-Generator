0b113da74d5c8d3fe626566e2e4c71e2c5126451
samstan092 <samstan092@gmail.com>
2024-10-22 15:06:13 -0500

    added generate markdown code and index.js code

---
+++ README.md
@@ -0,0 +1,39 @@
+# README Generator
+  ## Description
+  Takes user input and generates a 
+  ## Badges
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  ##### Table of Contents
+ - [Installation](#installation)
+ - [Usage](#usage)
+ - [Credits](#questions)
+ - [License](#license)
+  
+
+  ## Installation
+  From my username on github
+  ## Usage
+  Run the index.js file and type in the input
+  ## License
+  
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), 
+    to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+    and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  https://opensource.org/licenses/MIT
+
+  ## How to Contribute
+  Contact me from the questions section
+  ## Tests
+  Use the project
+  ## Questions
+  # Email
+  samstan092@gmail.com
+  # GitHub
+  samstan0926
+  
--- index.js
+++ index.js
@@ -1,13 +1,98 @@
 // TODO: Include packages needed for this application
 import inquirer from "inquirer";
+import fs from "fs";
+import { renderLicenseBadge, renderLicenseLink, renderLicenseSection, generateMarkdown } from "./generateMarkdown.js";
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is the project title?", 
+    "What is the project description?",
+    "How do you install your project?",
+    "What are the instructions to use your project?",
+    "How can other developers contribute?",
+    "How can you run tests on this project?",
+    "What is your GitHub username?",
+    "What is your email?",
+    "What license did you use?"
+];
+function init() {
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: questions[0]
 
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: questions[1],
+        },
+
+        {
+            type: 'input',
+            name: 'installation',
+            message: questions[2]
+        },
+        {
+            type: 'input',
+            name: 'instructions',
+            message: questions[3]
+        },
+        {
+            type: 'input',
+            name: 'contribute',
+            message: questions[4]
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: questions[5]
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: questions[6]
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: questions[7]
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: questions[8],
+            choices: ['MIT', 'Apache 2.0', 'ISC', 'Boost']
+        }
+
+
+
+
+
+
+
+    ])
+    .then((data) => {
+        let fileData = generateMarkdown(data);
+        writeToFile('README.md', fileData);
+
+    })
+}
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("successfully created")
+        }
+    }
+
+    )
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+
 
 // Function call to initialize app
 init();
